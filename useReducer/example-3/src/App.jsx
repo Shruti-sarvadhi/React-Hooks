d3968f621ec427eb5c9e77111ee3c0ba6f38e3d1
Shruti-sarvadhi <shrutikanani.sarvadhi@gmail.com>
2025-03-04 14:56:01 +0530

    Shopping Cart useing useReducer

--- useReducer/example-3/src/App.jsx
+++ useReducer/example-3/src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useReducer } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState={
+  cart:[]
+}
 
+const reducer=(state,action)=>{
+  switch (action.type) {
+    case "ADD":return {cart:[...state.cart,action.payload]}
+    case 'REMOVE':
+      return { cart: state.cart.filter(item => item.id !== action.payload) };
+    case 'CLEAR':
+      return { cart: [] };
+    default:
+      return state;
+  }
+ 
+}
+
+const App = () => {
+  const [state,dispatch]=useReducer(reducer,initialState)
+
+  const products = [
+    { id: 1, name: 'Shirt', price: 20 },
+    { id: 2, name: 'Pants', price: 30 }
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      {products.map(product => (
+        <button
+          key={product.id}
+          onClick={() => dispatch({ type: 'ADD', payload: product })}
+        >
+          Add {product.name} (${product.price})
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      ))}
+
+        <h2>Cart</h2>
+        <ul>
+        {state.cart.map(item => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+            <button onClick={() => dispatch({ type: 'REMOVE', payload: item.id })}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => dispatch({ type: 'CLEAR' })}>Clear Cart</button>
+    </div>
   )
 }
 
