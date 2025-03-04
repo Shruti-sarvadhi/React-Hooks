e7ae26469939f8658836687101b2558d9e12954e
Shruti-sarvadhi <shrutikanani.sarvadhi@gmail.com>
2025-03-04 17:39:20 +0530

    Scroll Position Management

--- useRef/example-7/src/App.jsx
+++ useRef/example-7/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const divRef = useRef(null);
+
+  const scrollToTop = () => {
+    divRef.current.scrollTop = 0;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div 
+        ref={divRef}
+        style={{ height: '500px' ,width:"100vw", overflow: 'auto' }}
+      >
+        <div style={{ height: '1000px' }}>
+          Scroll me!
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <button onClick={scrollToTop}>Scroll to Top</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
