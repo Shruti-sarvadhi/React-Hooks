d1d1972cf683637a971191855536c752fedb8262
Shruti-sarvadhi <shrutikanani.sarvadhi@gmail.com>
2025-03-04 17:52:05 +0530

    video player controls

--- useRef/example-10/src/App.jsx
+++ useRef/example-10/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef(null);
+
+  const playVideo = () => videoRef.current.play();
+  const pauseVideo = () => videoRef.current.pause();
 
   return (
-    <>
+    <div>
+      <video 
+        ref={videoRef}
+        width="400"
+        controls
+      >
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+      </video>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={playVideo}>Play</button>
+        <button onClick={pauseVideo}>Pause</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
